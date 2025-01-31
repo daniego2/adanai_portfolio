ea6184badb44cf399391b21afbfc756ae90b9aeb
daniego2 <danigonzalez282@gmail.com>
2025-01-31 13:49:20 +0100

    music-player proportions hardcoded. Youtube integration. Overflow for albums

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// Función para cambiar el álbum en el reproductor
-function loadAlbum(albumId, imageUrl) {
-    const iframe = document.getElementById('bandcamp-player');
-    
-    // Cambiar la URL del iframe con el nuevo álbum
-    iframe.src = `https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/`;
-    
-    // Cambiar la imagen de la portada del álbum
-    const albumImage = document.querySelector('.album-image');
-    albumImage.src = imageUrl;
-  }
-  
+// Función para mostrar el reproductor
+function showPlayer() {
+  const player = document.querySelector('.music-player');
+  player.style.display = 'block';
+}
+
+// Función para cargar un álbum de Bandcamp
+function loadBandcampAlbum(albumId) {
+  const iframe = document.getElementById('bandcamp-player');
+  iframe.src = `https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/`;
+  showPlayer();
+}
+
+// Función para cargar un video de YouTube
+function loadYouTubeVideo(videoId) {
+  const iframe = document.getElementById('bandcamp-player');
+  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+  showPlayer();
+}
