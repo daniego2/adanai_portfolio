ab19e1bec769cdb2485f5001c17ebf0ef8ec6eb3
daniego2 <danigonzalez282@gmail.com>
2025-02-02 02:10:03 +0100

    about section in index. player responds to aspect-ratio changes (still a bit hardcoded)

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // Función para mostrar el reproductor
 function showPlayer() {
-  const player = document.querySelector('.music-player');
+  const player = document.querySelector('.player');
   player.style.display = 'block';
 }
 
 // Función para cargar un álbum de Bandcamp
 function loadBandcampAlbum(albumId) {
-  const iframe = document.getElementById('bandcamp-player');
+  const iframe = document.getElementById('player');
   iframe.src = `https://bandcamp.com/EmbeddedPlayer/album=${albumId}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/`;
+  iframe.style.width = '25%';  // Hardcodeado el tamaño del reproductor
   showPlayer();
 }
 
 // Función para cargar un video de YouTube
 function loadYouTubeVideo(videoId) {
-  const iframe = document.getElementById('bandcamp-player');
+  const iframe = document.getElementById('player');
+  iframe.style.width = '50%'; // Hardcodeado el tamaño del reproductor
   iframe.src = `https://www.youtube.com/embed/${videoId}`;
   showPlayer();
 }
