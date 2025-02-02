00134c047c74c113b8497d1bf56be6ee124e54a5
daniego2 <danigonzalez282@gmail.com>
2025-02-02 22:45:20 +0100

    add description in json

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
       albums.forEach(album => {
         if (categories[album.category]) {
           const albumElement = document.createElement("li");
+          album.title = album.title.replace(/'/g, "\\'"); // Escapar las comillas simples y que no de problemas en el onclick
           albumElement.innerHTML = `
-            <a href="#" onclick="loadMedia('${album.id}', '${album.type}')">
+            <a href="#" onclick="loadMedia('${album.id}', '${album.type}', '${album.title}', '${album.description}')">
               <img src="${album.image}" alt="${album.title}" />
             </a>
           `;
@@ -25,9 +26,20 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Función genérica para cargar álbumes o videos en el reproductor
-function loadMedia(id, type) {
+function loadMedia(id, type, title, description) {
   const iframe = document.getElementById("player");
+  const descriptionContainer = document.getElementById("description-container");
+  const albumTitle = document.getElementById("album-title");
+  const albumDescription = document.getElementById("album-description");
+
+
+
   iframe.style.display = "block";
+  descriptionContainer.style.display = "block"; // Make the container visible
+  albumTitle.textContent = title;
+  albumDescription.textContent = description;
+
+
 
   if (type === "bandcamp") {
     iframe.src = `https://bandcamp.com/EmbeddedPlayer/album=${id}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/`;
